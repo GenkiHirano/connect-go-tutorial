269853836b7abd9e5eeec31b91eba77008e20b1f
GenkiHirano <genki86web@gmail.com>
2022-11-29 23:33:33 +0900

    refactor: connect-goでエラーをラップした実装例を追加

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"example/gen/greet/v1/greetv1connect"
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/bufbuild/connect-go"
@@ -16,31 +14,31 @@ import (
 
 type GreetServer struct{}
 
-func (s *GreetServer) Greet(ctx context.Context, req *connect.Request[greetv1.GreetRequest]) (*connect.Response[greetv1.GreetResponse], error) {
-	log.Println("Request headers: ", req.Header())
-	res := connect.NewResponse(&greetv1.GreetResponse{
-		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
-	})
-	res.Header().Set("Greet-Version", "v1")
-	return res, nil
-}
-
-// // connect-goでエラーをラップした実装例
 // func (s *GreetServer) Greet(ctx context.Context, req *connect.Request[greetv1.GreetRequest]) (*connect.Response[greetv1.GreetResponse], error) {
-// 	if err := ctx.Err(); err != nil {
-// 		return nil, err // automatically coded correctly
-// 	}
-// 	if err := validateGreetRequest(req.Msg); err != nil {
-// 		return nil, connect.NewError(connect.CodeInvalidArgument, err)
-// 	}
-// 	greeting, err := doGreetWork(ctx, req.Msg)
-// 	if err != nil {
-// 		return nil, connect.NewError(connect.CodeUnknown, err)
-// 	}
-// 	return connect.NewResponse(&greetv1.GreetResponse{
-// 		Greeting: greeting,
-// 	}), nil
+// 	log.Println("Request headers: ", req.Header())
+// 	res := connect.NewResponse(&greetv1.GreetResponse{
+// 		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
+// 	})
+// 	res.Header().Set("Greet-Version", "v1")
+// 	return res, nil
 // }
+
+// connect-goでエラーをラップした実装例
+func (s *GreetServer) Greet(ctx context.Context, req *connect.Request[greetv1.GreetRequest]) (*connect.Response[greetv1.GreetResponse], error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err // automatically coded correctly
+	}
+	if err := validateGreetRequest(req.Msg); err != nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, err)
+	}
+	greeting, err := doGreetWork(ctx, req.Msg)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeUnknown, err)
+	}
+	return connect.NewResponse(&greetv1.GreetResponse{
+		Greeting: greeting,
+	}), nil
+}
 
 func main() {
 	greeter := &GreetServer{}
